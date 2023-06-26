854579b4f3c391fb4d9c2d4002818a4bfd69669d
wxmbugu <wambugumacharia35@gmail.com>
2023-06-26 22:45:35 +0300

    fixes
    Signed-off-by: wxmbugu <wambugumacharia35@gmail.com>

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -9,18 +9,42 @@
           <div class="flex space-x-4">
             <!--Logo -->
             <a href="" class="flex items-center space-x-2 py-5 px-2">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
-              </svg><router-link :to="{ name: 'Home' }" class="text-2xl font-extrabold text-white">Elearning</router-link>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-8 w-8 text-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
+                /></svg
+              ><router-link
+                :to="{ name: 'Home' }"
+                class="text-2xl font-extrabold text-white"
+                >Interview</router-link
+              >
             </a>
           </div>
           <!-- Login -->
-          <div v-if="!this.$store.state.user" class="hidden md:flex items-center space-x-1">
-            <router-link :to="{ name: 'Login' }" class="py-5 px-2 text-white hover:text-indigo-200">Login</router-link>
-            <router-link :to="{ name: 'Register' }" href="" class="py-5 px-2 text-white hover:text-indigo-200">Sign
-              Up</router-link>
+          <div
+            v-if="!this.$store.state.user"
+            class="hidden md:flex items-center space-x-1"
+          >
+            <router-link
+              :to="{ name: 'Login' }"
+              class="py-5 px-2 text-white hover:text-indigo-200"
+              >Login</router-link
+            >
+            <router-link
+              :to="{ name: 'Register' }"
+              href=""
+              class="py-5 px-2 text-white hover:text-indigo-200"
+              >Sign Up</router-link
+            >
           </div>
           <div v-else>
             <Pmenu class="py-6 float-right"></Pmenu>
@@ -28,9 +52,19 @@
           <!-- Show Mobile menu -->
           <div class="md:hidden flex items-center">
             <button @click="showmobilemenu = !showmobilemenu" class="py-5 px-2">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-indigo-100" fill="none" viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-8 w-8 text-indigo-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
               </svg>
             </button>
           </div>
